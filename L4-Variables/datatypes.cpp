6627eb61064842eda8c2189056075fd6fbe7a679
sanjay kumar <sanjaykumarjasiwal61@gmail.com>
2024-11-09 00:20:35 +0530

    typecasting

--- L4-Variables/datatypes.cpp
+++ L4-Variables/datatypes.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     const int a=5;
     // a=8;
-    // we cant assign any value ot a variable as it is const.
-    
+    // we cant assign any value it a variable as it is const.
+
     cout<<a;
 }
---
+++ L4-Variables/typecasting.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    /*Typecasting:
+
+    Widening typecasting
+    Narrowing typecasting
+
+    widening typecasting:
+      -there is no data loss
+      -small sized datatype is being converted to big sized datatype
+
+    Narrowing Typecasting:
+      -there is data loss
+      -big sized datatype is converted to small sized datatype.
+
+    */
+
+   int a = 7;
+   double b=a;
+   cout<<"a : "<<a<<endl;
+   cout<<"b : "<<b<<endl;
+
+   double c=7.8;
+   int d=(int) c;
+   cout<<"c : "<<c<<endl;
+   cout<<"d : "<<d<<endl;
+
+   //conversion to string:
+   string s1=to_string(a);
+   cout<<s1<<endl;
+
+   //string to integer conversion
+   string f="12";
+   string g="1";
+   int h=stoi(f);
+   int i=stoi(g);
+   cout<<(h+i);
+
+  return 0;
+}
