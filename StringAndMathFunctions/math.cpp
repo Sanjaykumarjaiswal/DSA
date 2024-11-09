a39ad48ccf86d2342ea4e421fdc6a343c8289915
sanjay kumar <sanjaykumarjasiwal61@gmail.com>
2024-11-10 00:52:31 +0530

    math functions

--- StringAndMathFunctions/math.cpp
+++ StringAndMathFunctions/math.cpp
@@ -2,5 +2,26 @@
 #include<math.h>
 using namespace std;
 int main(){
-    
+    int a=4;
+    int b=6;
+    cout<<min(a,b)<<endl;
+
+    //to get square root
+    cout<<"square root of a:"<<sqrt(a)<<endl;
+
+    cout<<"M_E:"<<M_E<<endl;
+    cout<<"M_PI:"<<M_PI<<endl;
+    cout<<"max of a and b:"<<max(a,b)<<endl;
+    cout<<"cube root of 8:"<<cbrt(8)<<endl;
+    cout<<"2^3:"<<pow(2,3)<<endl;
+    cout<<"abs(3):"<<abs(3)<<endl;
+    cout<<"abs(-5):"<<abs(-5)<<endl;
+    cout<<"floor(-3.1):"<<floor(-3.1)<<endl;
+    cout<<"floor(9.8):"<<floor(9.8)<<endl;
+    cout<<"ceil(2.95):"<<ceil(2.95)<<endl;
+    cout<<"ceil(-1.5):"<<ceil(-1.5)<<endl;
+
+
+    return 0;
+
 }
