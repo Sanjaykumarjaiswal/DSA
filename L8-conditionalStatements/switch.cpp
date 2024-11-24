5d250c09a30912b1ecf55548744e5c4aef09f3f2
sanjay kumar <sanjaykumarjasiwal61@gmail.com>
2024-11-24 09:29:45 +0530

    switch

---
+++ L8-conditionalStatements/switch.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    switch(num){
+        case 1:
+        cout<<"a";
+        case 2:
+        cout<<"b";
+        case 3:
+        cout<<"c";
+        default:
+        cout<<"d";
+    }
+}
